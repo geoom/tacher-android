cd7c03cc433789db228bd5875a50848f27d9e5b8
YUNIOR <escorjunnior@gmail.com>
2014-08-31 08:09:02 -0500

    profile create

---
+++ app/src/main/java/me/doapps/tacher/Profile.java
@@ -0,0 +1,16 @@
+package me.doapps.tacher;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by YUNIOR on 31/08/2014.
+ */
+public class Profile extends ActionBarActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+}
