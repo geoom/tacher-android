b2d6a15fb946cdda7f43bf97ecda29a57b555fb5
jnolascob <jnolascob@gmail.com>
2014-08-31 03:56:33 -0500

    timer

--- app/src/main/java/me/doapps/tacher/Main.java
+++ app/src/main/java/me/doapps/tacher/Main.java
@@ -1,17 +1,35 @@
 package me.doapps.tacher;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class Main extends ActionBarActivity {
+    private static final long SPLASH_SCREEN_DELAY = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Main.this, Search.class));
+                finish();
+            }
+
+        };
+
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
     }
 
 
---
+++ app/src/main/java/me/doapps/tacher/Search.java
@@ -0,0 +1,15 @@
+package me.doapps.tacher;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by jnolascob on 31/08/2014.
+ */
+public class Search extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
