a76e2262f2a7d1b54b8906c6471cbbfaa103d35a
jnolascob <jnolascob@gmail.com>
2014-09-08 00:25:26 -0500

    add login with facebook

--- app/src/main/java/me/doapps/tacher/Main.java
+++ app/src/main/java/me/doapps/tacher/Main.java
@@ -1,10 +1,14 @@
 package me.doapps.tacher;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.Timer;
@@ -21,25 +25,34 @@ public class Main extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        getActionBar().hide();
+
         /*sincronizamos*/
-        TextView txt_titulo = (TextView)findViewById(R.id.txt_titulo);
+        TextView txt_titulo = (TextView) findViewById(R.id.txt_titulo);
         txt_titulo.setTypeface(Fonts.setAngryBird(Main.this));
 
-        TextView txt_subtitulo = (TextView)findViewById(R.id.txt_subtitulo);
+        TextView txt_subtitulo = (TextView) findViewById(R.id.txt_subtitulo);
         txt_subtitulo.setTypeface(Fonts.setAngryBird(Main.this));
 
-        TimerTask task = new TimerTask() {
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(Main.this, Search.class));
-                finish();
+                findViewById(R.id.progress_splash).setVisibility(View.GONE);
+                findViewById(R.id.btn_login_fb).setVisibility(View.VISIBLE);
+                findViewById(R.id.btn_login_fb).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(Main.this,Search.class);
+                        startActivity(i);
+                        finish();
+
+                    }
+                });
+
             }
+        }, 3000);
 
-        };
 
-        // Simulate a long loading process on application startup.
-        Timer timer = new Timer();
-        timer.schedule(task, SPLASH_SCREEN_DELAY);
     }
 
 
